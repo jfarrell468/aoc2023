4e6975ee5afb8a5566281d665e04838520f9ad32
James Farrell <jamesfarrell@google.com>
2023-12-04 10:06:21 -0600

    1.1 correct

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+    for line in std::io::stdin().lines() {
+        let mut first = None;
+        let mut last = 0;
+        for c in line.unwrap().bytes() {
+            if !c.is_ascii_digit() {
+                continue;
+            }
+            if first.is_none() {
+                first = Some((c - b'0') as u32);
+            }
+            last = (c - b'0') as u32;
+        }
+        sum += 10 * first.unwrap() + last;
+    }
+    println!("{}", sum);
 }
