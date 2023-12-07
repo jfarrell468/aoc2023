07983895e75e516e286d5c7db9c170e011f80d38
James Farrell <jamesfarrell@google.com>
2023-12-07 11:48:09 -0600

    Improved template.

--- aoc25/src/main.rs
+++ aoc25/src/main.rs
@@ -1,9 +1,45 @@
+use anyhow::Result;
 use util::input_lines;
 
-fn main() {
+fn part1(lines: &Vec<String>) -> Result<u32> {
+    Ok(0)
+}
+
+fn part2(lines: &Vec<String>) -> Result<u32> {
+    Ok(0)
+}
+
+fn main() -> Result<()> {
     let lines = input_lines();
+    println!("Part 1: {}", part1(&lines)?);
+    println!("Part 2: {}", part2(&lines)?);
+    Ok(())
+}
 
-    println!("Part 1: {}", todo!());
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
 
-    println!("Part 2: {}", todo!());
+    use super::*;
+
+    fn lines() -> Vec<String> {
+        indoc! {"
+            // TODO: example input
+        "}
+        .lines()
+        .map(|x| x.to_string())
+        .collect()
+    }
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        assert_eq!(part1(&lines())?, 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<()> {
+        assert_eq!(part2(&lines())?, 0);
+        Ok(())
+    }
 }
